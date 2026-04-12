bc5cf2241003c9ec17a28559d982e8c89a67c1c4
Prabhnoor0 <prabhnooorkaur11@gmail.com>
2026-04-13 00:28:30 +0530

    added logic

---
+++ Currency/logic.swift
@@ -0,0 +1,24 @@
+//
+//  logic.swift
+//  Currency
+//
+//  Created by Prabhnoor Kaur on 12/04/26.
+//
+
+import Foundation
+
+// Create Data Model
+
+// Create API fetch function
+func fetchExchangeRates() async {
+    do {
+        // Initialize URL
+
+        // Make the API call and decode response
+
+        // Decode data into your model and update UI
+
+    } catch {
+        print("API_ERROR: \(error.localizedDescription)")
+    }
+}
